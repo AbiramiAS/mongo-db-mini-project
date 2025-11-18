d82d46821db512c3529fe17cbb739920ac1cc540
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-18 15:30:45 +0530

    Refresh controller change to connect to DB

--- controllers/refreshController.js
+++ controllers/refreshController.js
@@ -1,35 +1,28 @@
-import { userData } from "../model/users.js";
+import UserData from "../model/users.js";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 
-const userDB = {
-  users: userData,
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
-dotenv.config();
-export const getRefreshToken = (req, res) => {
+export const getRefreshToken = async (req, res) => {
   const cookies = req.cookies;
+;
   if (!cookies?.jwt) return res.sendStatus(401);
   const refreshToken = cookies.jwt;
-  const existingUser = userDB.users.find(
-    (data) => data.refreshToken === refreshToken
-  ); 
+
+  const existingUser = await UserData.findOne(
+    { refreshToken }).exec();
+
   if (!existingUser) return res.sendStatus(403);
 
   //JWT token verification
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
     if (err || existingUser.username !== decoded.username)
       return res.sendStatus(403);
-    const userRole =  Object.values(existingUser.roles) ;
+    const userRoles = Object.values(existingUser.roles);
     const accessToken = jwt.sign(
-      { "User Details": { username: existingUser.username, roles: userRole } },
+      { "User Details": { username: existingUser.username, roles: userRoles } },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "90s" }
     );
-    res.json({ accessToken });
+    res.json({userRoles, accessToken });
   });
 };
 export default getRefreshToken;
